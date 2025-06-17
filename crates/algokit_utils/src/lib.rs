fc8f7bc8101bc9926839309be15bc8f86cf3406b
Joe Polny <joepolny@gmail.com>
2025-06-17 14:31:24 -0400

    wip: add_payment in rust utils

--- crates/algokit_utils/src/lib.rs
+++ crates/algokit_utils/src/lib.rs
@@ -1,8 +1,10 @@
 use std::sync::Arc;
 
 use algokit_http_client_trait::{HttpClient, HttpError};
-use algokit_transact::AlgorandMsgpack;
+use algokit_transact::Address;
+use algokit_transact::SignedTransaction;
 use algokit_transact::Transaction;
+use derive_more::Debug;
 
 #[cfg(feature = "default_http_client")]
 use algokit_http_client_trait::DefaultHttpClient;
@@ -31,16 +33,52 @@ impl AlgodClient {
     }
 }
 
+#[derive(Debug)]
+pub struct TransactionParams {
+    pub sender: Address,
+    #[debug(skip)]
+    pub signer: Option<TxnSigner>,
+    pub rekey_to: Option<Address>,
+    pub note: Option<Vec<u8>>,
+    pub lease: Option<[u8; 32]>,
+    pub static_fee: Option<u64>,
+    pub extra_fee: Option<u64>,
+    pub max_fee: Option<u64>,
+    pub validity_window: Option<u64>,
+    pub first_valid_round: Option<u64>,
+    pub last_valid_round: Option<u64>,
+}
+
+#[derive(Debug)]
+pub struct PaymentParams {
+    pub common_fields: TransactionParams,
+    pub receiver: Address,
+    pub amount: u64,
+    pub close_remainder_to: Option<Address>,
+}
+
+#[derive(Debug)]
+pub enum ComposerTxn {
+    Transaction(Transaction),
+    SignedTransaction(SignedTransaction),
+    Payment(PaymentParams),
+}
+
+type TxnSigner = Arc<dyn Fn(&[Transaction], &[usize]) -> Vec<SignedTransaction> + Send + Sync>;
+type TxnSignerGetter = Arc<dyn Fn(Address) -> Option<TxnSigner> + Send + Sync>;
+
 pub struct Composer {
-    transactions: Vec<Transaction>,
+    transactions: Vec<ComposerTxn>,
     algod_client: AlgodClient,
+    get_signer_fn: TxnSignerGetter,
 }
 
 impl Composer {
-    pub fn new(algod_client: AlgodClient) -> Self {
+    pub fn new(algod_client: AlgodClient, get_signer: Option<TxnSignerGetter>) -> Self {
         Composer {
             transactions: Vec::new(),
             algod_client: algod_client,
+            get_signer_fn: get_signer.unwrap_or(Arc::new(|_| None)),
         }
     }
 
@@ -49,29 +87,35 @@ impl Composer {
         Composer {
             transactions: Vec::new(),
             algod_client: AlgodClient::testnet(),
+            get_signer_fn: Arc::new(|_| None),
         }
     }
 
-    pub fn add_transaction(&mut self, transaction: Transaction) -> Result<(), String> {
+    fn push(&mut self, txn: ComposerTxn) -> Result<(), String> {
         if self.transactions.len() >= 16 {
-            return Err("Cannot add more than 16 transactions to a Composer".to_string());
+            return Err("Composer can only hold up to 16 transactions".to_string());
         }
-        self.transactions.push(transaction);
+        self.transactions.push(txn);
         Ok(())
     }
 
-    pub fn encode(&self) -> Result<Vec<Vec<u8>>, String> {
-        self.transactions
-            .iter()
-            .map(|tx| tx.encode())
-            .collect::<Result<Vec<_>, _>>()
-            .map_err(|e| e.to_string())
+    pub fn add_payment(&mut self, payment_params: PaymentParams) -> Result<(), String> {
+        self.push(ComposerTxn::Payment(payment_params))
     }
 
-    pub fn transactions(&self) -> Vec<Transaction> {
-        self.transactions.clone()
+    pub fn add_transaction(&mut self, transaction: Transaction) -> Result<(), String> {
+        self.push(ComposerTxn::Transaction(transaction))
     }
 
+    pub fn transactions(&self) -> &Vec<ComposerTxn> {
+        &self.transactions
+    }
+
+    pub fn get_signer(&self, address: Address) -> Option<TxnSigner> {
+        (self.get_signer_fn)(address)
+    }
+
+    // TODO: Use Fn defined in ComposerConfig
     pub async fn get_suggested_params(&self) -> Result<String, HttpError> {
         Ok(self.algod_client.get_suggested_params().await?)
     }
@@ -80,7 +124,7 @@ impl Composer {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use algokit_transact::test_utils::TransactionMother;
+    use algokit_transact::test_utils::{AddressMother, TransactionMother};
     use tokio;
 
     #[test]
@@ -101,18 +145,6 @@ mod tests {
         assert!(composer.add_transaction(txn).is_err());
     }
 
-    #[test]
-    fn test_encode_transactions() {
-        let mut composer = Composer::testnet();
-        for _ in 0..5 {
-            let txn = TransactionMother::simple_payment().build().unwrap();
-            assert!(composer.add_transaction(txn).is_ok());
-        }
-        let encoded = composer.encode();
-        assert!(encoded.is_ok());
-        assert_eq!(encoded.unwrap().len(), 5);
-    }
-
     #[tokio::test]
     async fn test_get_suggested_params() {
         let composer = Composer::testnet();
@@ -121,5 +153,29 @@ mod tests {
         assert!(
             response.contains(r#""genesis-hash":"SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI=""#)
         )
+    }
+
+    #[test]
+    fn test_add_payment() {
+        let mut composer = Composer::testnet();
+        let payment_params = PaymentParams {
+            common_fields: TransactionParams {
+                sender: AddressMother::address(),
+                signer: None,
+                rekey_to: None,
+                note: None,
+                lease: None,
+                static_fee: None,
+                extra_fee: None,
+                max_fee: None,
+                validity_window: None,
+                first_valid_round: None,
+                last_valid_round: None,
+            },
+            receiver: AddressMother::address(),
+            amount: 1000,
+            close_remainder_to: None,
+        };
+        assert!(composer.add_payment(payment_params).is_ok());
     }
 }
