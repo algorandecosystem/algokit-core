13a69c7efa8e6180ebf1cdf574d405e9f4dc002e
Joe Polny <joepolny@gmail.com>
2025-06-17 17:03:02 -0400

    wip: build() function

--- crates/algokit_utils/src/lib.rs
+++ crates/algokit_utils/src/lib.rs
@@ -3,12 +3,17 @@ use algod_api::AlgodClient;
 use algod_api::TransactionParams;
 use algokit_http_client_trait::HttpError;
 use algokit_transact::Address;
+use algokit_transact::FeeParams;
+use algokit_transact::PaymentTransactionFields;
 use algokit_transact::SignedTransaction;
 use algokit_transact::Transaction;
+use algokit_transact::TransactionHeader;
+use base64::DecodeError;
+use base64::{Engine as _, engine::general_purpose};
 use derive_more::Debug;
 use std::sync::Arc;
 
-#[derive(Debug)]
+#[derive(Debug, Default, Clone)]
 pub struct CommonParams {
     pub sender: Address,
     #[debug(skip)]
@@ -32,11 +37,20 @@ pub struct PaymentParams {
     pub close_remainder_to: Option<Address>,
 }
 
+// TODO: TransactionWithSigner
 #[derive(Debug)]
 pub enum ComposerTxn {
     Transaction(Transaction),
-    SignedTransaction(SignedTransaction),
     Payment(PaymentParams),
+}
+
+impl ComposerTxn {
+    pub fn common_params(&self) -> CommonParams {
+        match self {
+            ComposerTxn::Payment(payment_params) => payment_params.common_params.clone(),
+            _ => CommonParams::default(),
+        }
+    }
 }
 
 type TxnSigner = Arc<dyn Fn(&[Transaction], &[usize]) -> Vec<SignedTransaction> + Send + Sync>;
@@ -46,6 +60,7 @@ pub struct Composer {
     transactions: Vec<ComposerTxn>,
     algod_client: AlgodClient,
     get_signer_fn: TxnSignerGetter,
+    built_group: Option<Vec<Transaction>>,
 }
 
 impl Composer {
@@ -54,7 +69,12 @@ impl Composer {
             transactions: Vec::new(),
             algod_client: algod_client,
             get_signer_fn: get_signer.unwrap_or(Arc::new(|_| None)),
+            built_group: None,
         }
+    }
+
+    pub fn built_group(&self) -> Option<&Vec<Transaction>> {
+        self.built_group.as_ref()
     }
 
     #[cfg(feature = "default_http_client")]
@@ -63,6 +83,7 @@ impl Composer {
             transactions: Vec::new(),
             algod_client: AlgodClient::testnet(),
             get_signer_fn: Arc::new(|_| None),
+            built_group: None,
         }
     }
 
@@ -94,8 +115,81 @@ impl Composer {
     pub async fn get_suggested_params(&self) -> Result<TransactionParams, HttpError> {
         self.algod_client.transaction_params().await
     }
-}
 
+    pub async fn build(&mut self) -> Result<&mut Self, HttpError> {
+        let suggested_params = self.get_suggested_params().await?;
+
+        let default_header = TransactionHeader {
+            fee: Some(suggested_params.fee),
+            genesis_id: Some(suggested_params.genesis_id),
+            genesis_hash: Some(
+                general_purpose::STANDARD
+                    .decode(suggested_params.genesis_hash)
+                    .map_err(|e: DecodeError| {
+                        HttpError::HttpError(format!("Failed to decode genesis hash: {}", e))
+                    })?
+                    .try_into()
+                    .map_err(|e| {
+                        HttpError::HttpError(format!("Failed to convert genesis hash: {:?}", e))
+                    })?,
+            ),
+            // The rest of these fields are set further down per txn
+            first_valid: 0,
+            last_valid: 0,
+            sender: Address::default(),
+            rekey_to: None,
+            note: None,
+            lease: None,
+            group: None,
+        };
+
+        self.built_group = Some(
+            self.transactions
+                .iter()
+                .map(|composer_txn| {
+                    let alread_formed_txn = matches!(composer_txn, ComposerTxn::Transaction(_));
+
+                    let mut transaction: algokit_transact::Transaction = match composer_txn {
+                        ComposerTxn::Transaction(txn) => txn.clone(),
+                        ComposerTxn::Payment(pay_params) => {
+                            let pay_params = PaymentTransactionFields {
+                                header: default_header.clone(),
+                                receiver: pay_params.receiver.clone(),
+                                amount: pay_params.amount,
+                                close_remainder_to: pay_params.close_remainder_to.clone(),
+                            };
+
+                            Transaction::Payment(pay_params)
+                        }
+                    };
+
+                    if !alread_formed_txn {
+                        let common_params = composer_txn.common_params();
+                        let header = transaction.header_mut();
+
+                        header.sender = common_params.sender;
+                        header.rekey_to = common_params.rekey_to;
+                        header.note = common_params.note;
+                        header.lease = common_params.lease;
+
+                        transaction
+                            .assign_fee(FeeParams {
+                                fee_per_byte: suggested_params.fee,
+                                min_fee: suggested_params.min_fee,
+                                extra_fee: common_params.extra_fee,
+                                max_fee: common_params.max_fee,
+                            })
+                            .map_err(|e| HttpError::HttpError(e.to_string()))?;
+                    }
+
+                    Ok(transaction)
+                })
+                .collect::<Result<Vec<Transaction>, HttpError>>()?,
+        );
+
+        Ok(self)
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -153,5 +247,31 @@ mod tests {
             close_remainder_to: None,
         };
         assert!(composer.add_payment(payment_params).is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_build_payment() {
+        let mut composer = Composer::testnet();
+        let payment_params = PaymentParams {
+            common_params: CommonParams {
+                sender: AddressMother::address(),
+                signer: None,
+                rekey_to: None,
+                note: None,
+                lease: None,
+                static_fee: None,
+                extra_fee: None,
+                max_fee: None,
+                validity_window: None,
+                first_valid_round: None,
+                last_valid_round: None,
+            },
+            receiver: AddressMother::address(),
+            amount: 1000,
+            close_remainder_to: None,
+        };
+        assert!(composer.add_payment(payment_params).is_ok());
+        assert!(composer.build().await.is_ok());
+        assert!(composer.built_group().is_some());
     }
 }
