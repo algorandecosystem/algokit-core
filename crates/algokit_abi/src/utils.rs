499154a6e878b512e126cb61ba98c510bbc0701b
Hoang Dinh <patrick.dinh@makerx.com.au>
2025-07-18 12:57:26 +1000

    chore: small review

--- crates/algokit_abi/src/utils.rs
+++ crates/algokit_abi/src/utils.rs
@@ -1,3 +1,4 @@
+// TODO: maybe refactor this to only fix length for bigint
 pub fn extend_bytes_to_length(bytes: &[u8], len: usize) -> Vec<u8> {
     let mut result = vec![0u8; len - bytes.len()];
     result.extend_from_slice(bytes);
