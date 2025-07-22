ffd163c35d880098ed6ac3479e3557620132392d
Al <aorumbayev@pm.me>
2025-07-22 14:22:31 +0200

    feat: ABI method signature parsing (#212)
    * feat: adds ABI method parsing and signature utilities
    
    Adds functionality to parse ABI method signatures, extract arguments,
    and generate method selectors.
    
    This change introduces the `ABIMethod` struct and related utilities for
    working with Algorand ABI method definitions. It enables parsing method
    signatures, extracting argument types and names, and generating the 4-byte
    method selector used for identifying the method during contract calls.
    This enables easier interaction with smart contracts using the ABI
    specification.
    
    * chore: refining names of method arg types
    
    * chore: clippy warnings
    
    * chore: pr suggestions
    
    * refactor: ABI method parsing and representation
    
    Moves ABI method-related structs and parsing logic into a dedicated `method.rs` file for better organization.
    
    This change improves code modularity and readability by isolating the ABI method functionality.
    
    Also, modifies `parse_tuple_content` to be public for use in `method.rs`

--- crates/algokit_abi/src/lib.rs
+++ crates/algokit_abi/src/lib.rs
@@ -2,9 +2,12 @@ pub mod abi_type;
 pub mod abi_value;
 pub mod constants;
 pub mod error;
+pub mod method;
 pub mod types;
 pub mod utils;
 
 pub use abi_type::ABIType;
 pub use abi_value::ABIValue;
 pub use error::ABIError;
+
+pub use method::{ABIMethod, ABIMethodArg, ABIMethodArgType, ABIReferenceType, ABITransactionType};
