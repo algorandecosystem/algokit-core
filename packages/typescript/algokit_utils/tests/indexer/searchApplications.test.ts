43dc10d0b087a04d70ba2d3968caa444f9f1de63
Altynbek Orumbayev <altynbek.orumbayev@makerx.com.au>
2025-09-26 00:44:07 +0200

    chore: fix wait for indexer test order

--- packages/typescript/algokit_utils/tests/indexer/searchApplications.test.ts
+++ packages/typescript/algokit_utils/tests/indexer/searchApplications.test.ts
@@ -5,14 +5,15 @@ import { createDummyApp, getIndexerEnv, waitForIndexerTransaction } from './help
 describe('Indexer search applications', () => {
   it('should search for applications', async () => {
     const { appId, txId } = await createDummyApp()
+
     const env = getIndexerEnv()
     const client = new IndexerClient({ baseUrl: env.indexerBaseUrl, headers: { 'X-Algo-API-Token': env.indexerApiToken ?? '' } })
-    const res = await client.searchForApplications()
-
-    expect(res).toHaveProperty('applications')
-    expect(res.applications && res.applications.length).toBeGreaterThan(0)
 
     await waitForIndexerTransaction(client, txId)
+
+    const res = await client.searchForApplications()
+    expect(res).toHaveProperty('applications')
+    expect(res.applications && res.applications.length).toBeGreaterThan(0)
 
     const appTxns = await client.searchForApplications({ applicationId: appId })
     expect(appTxns).toHaveProperty('applications')
