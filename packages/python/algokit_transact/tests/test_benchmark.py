9721b7ac1205455532ea5f99f7c96d665ad239fc
Joe Polny <joepolny@gmail.com>
2025-10-18 11:36:28 -0400

    chore: rename variables

--- packages/python/algokit_transact/tests/test_benchmark.py
+++ packages/python/algokit_transact/tests/test_benchmark.py
@@ -3,19 +3,19 @@ from algokit_transact.algokit_transact_ffi import decode_transaction, encode_tra
 from . import TEST_DATA
 from algosdk import encoding
 
-def core_to_sdk_transaction(core_txn):
-    encoded_txn = encode_transaction_raw(core_txn)
+def core_to_sdk_transaction(txn):
+    encoded_txn = encode_transaction_raw(txn)
     return encoding.msgpack_decode(base64.b64encode(encoded_txn))
 
 
-txn = TEST_DATA.app_create.transaction
-sdk_txn = core_to_sdk_transaction(txn)
+core_txn = TEST_DATA.app_create.transaction
+sdk_txn = core_to_sdk_transaction(core_txn)
 
 
 def test_bench_core_encode(benchmark):
     """Benchmark encoding a simple payment transaction"""
     def encode():
-        encode_transaction(txn)
+        encode_transaction(core_txn)
 
     benchmark(encode)
 
@@ -28,7 +28,7 @@ def test_bench_sdk_encode(benchmark):
 
 def test_bench_core_decode(benchmark):
     """Benchmark decoding a simple payment transaction"""
-    encoded_txn = encode_transaction(txn)
+    encoded_txn = encode_transaction(core_txn)
 
     def decode():
         decode_transaction(encoded_txn)
